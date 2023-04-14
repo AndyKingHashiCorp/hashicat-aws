f6532ca193346fbd215499c9d8267c74a130e0c0
root <andy.king@hashicorp.com>
2023-04-14 14:46:02 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,3 +1,7 @@
+module "s3-bucket" {
+  source  = "app.terraform.io/Instruqt-Andy-King-Learning/s3-bucket/aws"
+  version = "2.8.0"
+}
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
