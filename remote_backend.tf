e9135df1bc3596009ecf2beeee3f31528523ac89
root <andy.king@hashicorp.com>
2023-04-14 14:51:35 +0000

    Added s3 bucket module

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "Instruqt-Andy-King-Learning"
     workspaces {
       name = "hashicat-aws"
--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,15 +1,12 @@
 module "s3-bucket" {
   source  = "app.terraform.io/Instruqt-Andy-King-Learning/s3-bucket/aws"
   version = "2.8.0"
-}
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "bucket_andy"
-  acl    = "private"
+  bucket  = "my-s3-bucket"
+  acl     = "private"
 
   versioning = {
     enabled = true
   }
 
 }
+
