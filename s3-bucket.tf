d0a893eabe762fe846dfea58ec532e95216f5660
Andy King @ HashiCorp <126875389+AndyKingHashiCorp@users.noreply.github.com>
2023-09-21 13:02:53 +0100

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3-bucket" {
-  source  = "app.terraform.io/Instruqt-Andy-King-Learning/s3-bucket/aws"
+  source  = "app.terraform.io/s3-bucket/aws"
   version = "2.8.0"
   bucket  = "bucket-instruqt-andy-king-learning"
   acl     = "private"
